3ea961c61c08d61c09c94069decf387c6b9a91af
ymeine <yannick.meine@amadeus.com>
2013-09-20 15:53:40 +0200

    Now the HTML grammar is built anytime a change is made inside.
    To enable this, install the dev dependencies specified in teh package.json file and run `npm run-script watch`.
    
    Potentially it can be applied to other grammars if they follow the same file system layout coventions.

---
+++ grammar-builder.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+var peg = require('pegjs');
+
+
+
+function build(name) {
+	var basePath = 'app/node_modules/modes/' + name + '/parser/';
+	var options = require('./' + basePath + 'options');
+	var grammar = fs.readFileSync(basePath + 'grammar.pegjs', 'utf-8');
+
+	options.output = 'source';
+	var source = peg.buildParser(grammar, options);
+
+	fs.writeFileSync(basePath + 'grammar.js', source);
+}
+
+
+exports.build = build;
