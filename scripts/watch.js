215268ef0056aa89730b3438989906900c724e2d
ymeine <yannick.meine@amadeus.com>
2013-09-20 15:53:40 +0200

    Added several enhancements for development.
    These features are mainly accessible from the npm scripts feature.
    
    Here they are:
    
    - watch: you can now launch a watch process to rebuild your grammars anytime you change them
    - test: launches the tests with mocha
    - build: builds the package. Actually only build the grammars for now.
    - stop: sends the shutdown signal to the server
    
    Documentation has been updated accordingly.
    
    NB: for now grammars handled by the build and watch scripts are HTML and AT. But it's easy to add others by appending them in corresponding scripts.

---
+++ scripts/watch.js
@@ -0,0 +1,19 @@
+var chokidar = require('chokidar');
+
+var grammarBuilder = require('./grammar-builder');
+
+function createWatcher(grammar) {
+	var watcher = chokidar.watch('app/node_modules/modes/' + grammar + '/parser/grammar.pegjs', {
+		persistent: true
+	});
+
+	watcher.on('change', function(path) {
+		grammarBuilder.build(grammar);
+	});
+
+	return watcher;
+}
+
+createWatcher('html');
+createWatcher('at');
+
