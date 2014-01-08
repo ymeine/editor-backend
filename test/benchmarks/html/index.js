68d92402318b5e26aa01bec25b71b31dbd6db1f1
ymeine <yannick.meine@amadeus.com>
2014-01-08 14:42:58 +0100

    Added benchmarks.
    A new utility has been created in tests, in order to run benchmarks.
    
    This utility relies on a benchmarking library, and mainly takes care of getting the inputs and displaying the results.
    
    For now it can use two benchmarking libraries:
    
    - the renowned benchmark.js library
    - a custom one developed internally, to have more control on how to benchmark and get significant results
    
    It uses the second by default.
    
    Here are the changes that have been done along with that:
    
    - added npm script definition tun run the benchmarks
    - added development dependencies for testing, mainly benchmarking
    - added inputs to benchmark the parsers HTML and AT-HTML

---
+++ test/benchmarks/html/index.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+
+
+
+
+function readSource(name) {
+	return fs.readFileSync(__dirname + '/input/' + name + '.html', 'utf-8');
+}
+
+
+var name = 'HTML';
+
+var parsers = [
+	{name: 'normal', parser: require('../../../app/node_modules/modes/html/parser').parser}
+];
+
+var inputs = [
+	{name: 'small', source: readSource('small')}
+	,
+	{name: 'big', source: bigSource = readSource('big')}
+];
+
+
+
+exports.name = name;
+exports.inputs = inputs;
+exports.parsers = parsers;
